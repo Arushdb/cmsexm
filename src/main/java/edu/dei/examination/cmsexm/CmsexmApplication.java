550f45d48aa9ec0b0cbdfb7166a9c0d5f919d0a9
Arush Kumar <arushkumar023@gmail.com>
2021-10-28 13:31:55 +0530

    Initial Commit

---
+++ src/main/java/edu/dei/examination/cmsexm/CmsexmApplication.java
@@ -0,0 +1,13 @@
+package edu.dei.examination.cmsexm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsexmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsexmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dei/examination/cmsexm/CmsexmApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dei.examination.cmsexm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsexmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
