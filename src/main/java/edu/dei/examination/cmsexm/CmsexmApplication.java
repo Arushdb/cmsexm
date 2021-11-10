e67a9741dc48acda18408709060371a88892341a
Arush Kumar <arushkumar023@gmail.com>
2021-11-10 13:30:36 +0530

    changes for war file

--- src/main/java/edu/dei/examination/cmsexm/CmsexmApplication.java
+++ src/main/java/edu/dei/examination/cmsexm/CmsexmApplication.java
@@ -2,12 +2,21 @@ package edu.dei.examination.cmsexm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CmsexmApplication {
+public class CmsexmApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CmsexmApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		
+		return builder.sources(CmsexmApplication.class);
+	
+	}
 }
