05f03be7d9416aed14e0e5d1be19099f4027fa13
arushkumar023 <arushkumar023@gmail.com>
2026-02-17 13:47:19 +0530

    PHD Progress report entry in progress

--- src/main/java/edu/dei/examination/phd/repository/AttendenceRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.dei.examination.phd.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.dei.examination.phd.model.Attendence;
-
-public interface AttendenceRepository extends JpaRepository<Attendence,Integer> {
-
-}
